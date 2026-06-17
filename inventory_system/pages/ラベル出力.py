dda382543d33e61db23b4282a73bda9b05ab1abb
ysuga <yousuke.suga0227@gmail.com>
2026-06-17 23:09:17 +0900

    Update SHARK

--- inventory_system/pages/ラベル出力.py
+++ inventory_system/pages/ラベル出力.py
@@ -447,19 +447,19 @@ def make_label_png(item):
         barcode_img = make_barcode_png(item_code)
 
         # 横幅をラベルいっぱいに近づける
-        target_w = width_px - 20
+        target_w = width_px - 10
         ratio = target_w / barcode_img.width
         target_h = int(barcode_img.height * ratio)
 
         # 縦方向も少し大きくする
-        target_h = int(target_h * 1.2)
+        target_h = int(target_h * 1.3)
 
         barcode_img = barcode_img.resize((target_w, target_h))
 
         x = int((width_px - target_w) / 2)
 
         # 下ギリギリに配置
-        y_bar = height_px - target_h - 8
+        y_bar = height_px - target_h - 5
 
         img.paste(barcode_img, (x, y_bar))
 
